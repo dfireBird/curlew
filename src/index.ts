38a5f5d90c65f78a269bf29ca9e319b3e77ca4e7
dfireBird <aadhishrm11@gmail.com>
2020-11-05 15:54:57 +0530

    chore: add cors dependency

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { Routes } from "./routes";
 
 createConnection().then(async connection => {
 
     const app = express();
     app.use(express.json());
+    app.use(cors());
 
     Routes.forEach(route => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
